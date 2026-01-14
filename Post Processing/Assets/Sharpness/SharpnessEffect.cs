a447f411ce19e3cfb07c71aa32a8d354512f69a9
Vladyslav Michurin <goduster.mail@gmail.com>
2026-01-14 10:51:01 -0500

    Update
    Playing with sharpness and manga shader

---
+++ Post Processing/Assets/Sharpness/SharpnessEffect.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine;
+
+using CameraCommon;
+
+[RequireComponent(typeof(Camera))]
+[ExecuteInEditMode]
+public class SharpnessEffect : MonoBehaviour
+{
+    [Range(0, 10.0f)]
+    public float sharpness = 1;
+
+    [HideInInspector]
+    public Shader sharpness_Shader;
+    private Material sharpnessMaterial;
+
+    private void OnDisable()
+    {
+        sharpnessMaterial = null; 
+    }
+
+    private void OnRenderImage(RenderTexture _source, RenderTexture _destination)
+    {
+        if(!BB_Rendering.ShaderMaterialReady(sharpness_Shader, ref sharpnessMaterial))
+        {
+            Graphics.Blit(_source, _destination);
+            return;
+        }
+
+        sharpnessMaterial.SetFloat("_Sharpness", sharpness);
+
+        Graphics.Blit(_source, _destination, sharpnessMaterial);
+    }
+
+}
